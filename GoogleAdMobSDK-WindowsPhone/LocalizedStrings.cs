3ebb556b1484cd3ef216846d6e9836e810e30a82
Todd Patrick <todd.patrick@espn.com>
2013-10-24 10:12:46 -0400

    Initial commit

---
+++ GoogleAdMobSDK-WindowsPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GoogleAdMobSDK_WindowsPhone.Resources;
+
+namespace GoogleAdMobSDK_WindowsPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ GoogleAdMobSDK-WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using GoogleAdMobSDK_WindowsPhone.Resources;
+using GoogleAds;
+
+namespace GoogleAdMobSDK_WindowsPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            AdView bannerAd = new AdView
+            {
+                Format = AdFormats.Banner,
+                AdUnitID = "",
+                Width = 320,
+                Height = 50
+            };
+
+            //Set up delegates
+            bannerAd.ReceivedAd += bannerAd_ReceivedAd;
+            bannerAd.FailedToReceiveAd += bannerAd_FailedToReceiveAd;
+
+
+            AdRequest adRequest = new AdRequest();
+            adRequest.ForceTesting = true;
+
+            ContentPanel.Children.Add(bannerAd);
+            bannerAd.LoadAd(adRequest);
+        }
+
+        void bannerAd_ReceivedAd(object sender, AdEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void bannerAd_FailedToReceiveAd(object sender, AdErrorEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
